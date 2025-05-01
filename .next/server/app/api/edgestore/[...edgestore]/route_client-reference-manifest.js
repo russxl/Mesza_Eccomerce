eb1554c936967070de64753c5300d6f895a5c1b5
russxl <obatayrussell@gmail.com>
2025-05-01 21:32:26 +0800

    Update .gitignore to include trace and client reference manifest files

--- .next/server/app/api/edgestore/[...edgestore]/route_client-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
-globalThis.__RSC_MANIFEST["/api/edgestore/[...edgestore]/route"] = {"moduleLoading":{"prefix":"","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
